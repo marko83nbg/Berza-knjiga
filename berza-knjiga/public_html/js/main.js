253c84226f318d01332b8dc6a7fe0b26dcff1e54
marko83nbg <marko83nbg@gmail.com>
2017-03-30 16:14:18 +0200

    30.03.

--- berza-knjiga/public_html/js/main.js
+++ berza-knjiga/public_html/js/main.js
@@ -1,22 +1,55 @@
 $(document).ready(function () {
 
-
+//MAIN SLIDER ON INDEX PAGE
     $('.bxslider').bxSlider({
         auto: false
     });
 
-    $('ul.nav li.dropdown').hover(function () {
-        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
-    }, function () {
-        $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+
+// 5 STAR RATIO 
+    $(".ratio").rateYo({
+        rating: 3.6,
+        starWidth: "20px",
+        ratedFill: "#066083"
+        
     });
     
-    $('.book-slider').slick({
-  });
+    //OWL CAROUSEL SLIDER
+if($('.owl-carousel').length>0){
+    //CALL ON INDEX PAGE IN TABS
+      $('.tab-carousel').owlCarousel({
+        navText:['',''],
+        loop: false,
+        margin: 10,
+        responsiveClass: true,
+        nav: true,
+        responsive: {
+            0: {
+                items: 2
+                
+            },
+            768: {
+                items: 3
+            
+            },
+            992: {
+                items: 5
+               
+
+            },
+            1200: {
+                items: 6
+
+            }
+        }
+    });
+    
+    /*NEXT OWL CAROUSEL*/
+    
+    
+}
   
-  $("#rateYo").rateYo({
-    rating: 3.6
-  });
+
 
 
 
