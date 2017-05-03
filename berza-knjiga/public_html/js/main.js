e50b33fe37f8387cb849a97432d348654527e7d8
Marko <Marko@DESKTOP-V9CB01F>
2017-05-03 23:09:06 +0200

    03.05.2017.

--- berza-knjiga/public_html/js/main.js
+++ berza-knjiga/public_html/js/main.js
@@ -118,6 +118,41 @@ $(document).ready(function () {
                 }
             }
         });
+        
+        /*NEXT OWL CAROUSEL*/
+
+        $('.author-carousel').owlCarousel({
+            navText: ['', ''],
+            loop: false,
+            margin: 10,
+            responsiveClass: true,
+            nav: true,
+            dots: false,
+            items:5,
+            responsive: {
+                0: {
+                    items: 1
+
+                },
+                480: {
+                    items: 2
+
+                },
+                768: {
+                    items: 3
+
+                },
+                992: {
+                    items:5
+
+
+                },
+                1200: {
+                    items: 5
+
+                }
+            }
+        });
 
     }
 
